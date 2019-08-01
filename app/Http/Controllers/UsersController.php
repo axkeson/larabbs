667066872645528ac65e5744cfd1628cfe8ff8d2
axkeson <axkeson@gmail.com>
2019-08-01 11:00:21 +0800

    编辑个人资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UserRequest;
 use App\Models\User;
 
 class UsersController extends Controller
@@ -14,5 +15,28 @@ class UsersController extends Controller
     public function show(User $user)
     {
         return view('users.show', compact('user'));
+    }
+
+    /**
+     * @param User $user
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function edit(User $user)
+    {
+        return view('users.edit', compact('user'));
+    }
+
+    /**
+     * @param UserRequest $request
+     * @param User        $user
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function update(UserRequest $request, User $user)
+    {
+        $user->update($request->all());
+
+        return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
     }
 }
