5fa0d6db75523ffc102c9edba27282ca24ff056f
axkeson <axkeson@gmail.com>
2019-08-02 00:43:22 +0800

    删除评论

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -24,9 +24,8 @@ class ReplyObserver
      */
     public function created(Reply $reply)
     {
-        $reply->topic->reply_count = $reply->topic->replies->count();
-        $reply->topic->save();
-        
+        $reply->topic->updateReplyCount();
+
         // 通知话题作者有新的评论
         $reply->topic->user->notify(new TopicReplied($reply));
     }
@@ -37,5 +36,13 @@ class ReplyObserver
     public function updating(Reply $reply)
     {
         //
+    }
+
+    /**
+     * @param Reply $reply
+     */
+    public function deleted(Reply $reply)
+    {
+        $reply->topic->updateReplyCount();
     }
 }
