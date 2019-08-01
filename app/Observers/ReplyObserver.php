c2d98fa5cf82322ea9dda7364a9a79998e3c3d56
axkeson <axkeson@gmail.com>
2019-08-01 23:46:51 +0800

    用户可以发表评论

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -9,11 +9,26 @@ use App\Models\Reply;
 
 class ReplyObserver
 {
+    /**
+     * @param Reply $reply
+     */
     public function creating(Reply $reply)
     {
-        //
+        $reply->content = clean($reply->content, 'user_topic_body');
     }
 
+    /**
+     * @param Reply $reply
+     */
+    public function created(Reply $reply)
+    {
+        $reply->topic->reply_count = $reply->topic->replies->count();
+        $reply->topic->save();
+    }
+
+    /**
+     * @param Reply $reply
+     */
     public function updating(Reply $reply)
     {
         //
