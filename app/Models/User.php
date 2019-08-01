9e148d5e97fe4aba6b08bb78aa70f34cef140c75
axkeson <axkeson@gmail.com>
2019-08-01 14:06:22 +0800

    用户话题列表

--- app/Models/User.php
+++ app/Models/User.php
@@ -37,4 +37,12 @@ class User extends Authenticatable implements MustVerifyEmailContract
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function topics()
+    {
+        return $this->hasMany(Topic::class);
+    }
 }
