c5f62a25663c8ecbc20658bd2040ab3b6cdbcc7e
axkeson <axkeson@gmail.com>
2019-08-05 21:34:17 +0800

    user register using phone

--- routes/api.php
+++ routes/api.php
@@ -21,4 +21,7 @@ $api->version('v1', [
 ], function (\Dingo\Api\Routing\Router $api) {
     // 短信验证码
     $api->post('verificationCodes', 'VerificationCodesController@store')->name('api.verificationCodes.store');
+    
+    // 用户注册
+    $api->post('users', 'UsersController@store')->name('api.users.store');
 });
