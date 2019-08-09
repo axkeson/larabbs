1c76062dee780ed4057c148a37dddf5c373d8903
axkeson <axkeson@gmail.com>
2019-08-10 00:58:49 +0800

    replies store

--- routes/api.php
+++ routes/api.php
@@ -73,6 +73,9 @@ $api->version('v1', [
             $api->patch('topics/{topic}', 'TopicsController@update')->name('api.topics.update');
 
             $api->delete('topics/{topic}', 'TopicsController@destroy')->name('api.topics.destroy');
+
+            // 发布回复
+            $api->post('topics/{topic}/replies', 'RepliesController@store')->name('api.topics.replies.store');
         });
     });
 });
