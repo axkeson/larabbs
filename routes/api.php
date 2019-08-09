e2dc23ba85dc9d41dcd388757eb3eaf12a417e3d
axkeson <axkeson@gmail.com>
2019-08-09 22:40:25 +0800

    topic delete api

--- routes/api.php
+++ routes/api.php
@@ -65,6 +65,8 @@ $api->version('v1', [
             $api->post('topics', 'TopicsController@store')->name('api.topics.store');
 
             $api->patch('topics/{topic}', 'TopicsController@update')->name('api.topics.update');
+
+            $api->delete('topics/{topic}', 'TopicsController@destroy')->name('api.topics.destroy');
         });
     });
 });
