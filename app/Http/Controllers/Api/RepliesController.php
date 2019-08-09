8d8cbeb1cf5d0e2a828876bb6fd58466fe8bfd85
axkeson <axkeson@gmail.com>
2019-08-10 01:10:19 +0800

    replies index

--- app/Http/Controllers/Api/RepliesController.php
+++ app/Http/Controllers/Api/RepliesController.php
@@ -4,11 +4,39 @@ namespace App\Http\Controllers\Api;
 
 use App\Models\Topic;
 use App\Models\Reply;
+use App\Models\User;
 use App\Transformers\ReplyTransformer;
 use App\Http\Requests\Api\ReplyRequest;
+use Illuminate\Http\Request;
 
 class RepliesController extends Controller
 {
+    public function index(Topic $topic, Request $request)
+    {
+        app(\Dingo\Api\Transformer\Factory::class)->disableEagerLoading();
+
+        $replies = $topic->replies()->paginate(20);
+
+        if ($request->include) {
+            $replies->load(explode(',', $request->include));
+        }
+
+        return $this->response->paginator($replies, new ReplyTransformer());
+    }
+
+    public function userIndex(User $user, Request $request)
+    {
+        app(\Dingo\Api\Transformer\Factory::class)->disableEagerLoading();
+
+        $replies = $user->replies()->paginate(20);
+
+        if ($request->include) {
+            $replies->load(explode(',', $request->include));
+        }
+
+        return $this->response->paginator($replies, new ReplyTransformer());
+    }
+
     public function store(ReplyRequest $request, Topic $topic, Reply $reply)
     {
         $reply->content = $request->content;
