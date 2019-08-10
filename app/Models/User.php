d6ee54c2e59699e4a3d9fe1539c14b4756b6ce53
axkeson <axkeson@gmail.com>
2019-08-10 20:49:24 +0800

    weapp login

--- app/Models/User.php
+++ app/Models/User.php
@@ -26,7 +26,8 @@ class User extends Authenticatable implements MustVerifyEmailContract, JWTSubjec
      * @var array
      */
     protected $fillable = [
-        'name',  'phone', 'email', 'password', 'avatar', 'introduction', 'weixin_openid', 'weixin_unionid', 'registration_id'
+        'name',  'phone', 'email', 'password', 'avatar', 'introduction', 'weixin_openid', 'weixin_unionid', 'registration_id', 'weixin_session_key', 'weapp_openid',
+
     ];
 
     /**
